d04490339c0db2450fce08ba55297cc81eae032b
Nayeem Ahamed <nayeemar.ahamed@gmail.com>
2026-02-10 19:24:35 +0300

    Implement player movement and refactor game loop using sprite groups

--- main.py
+++ main.py
@@ -7,18 +7,22 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     dt = 0
     player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
-
+    
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         dt = clock.tick(60)/1000
-        player.update(dt)
+        updatable.update(dt)
         screen.fill("black")
-        player.draw(screen)
+        for dr in drawable:
+            dr.draw(screen)
         pygame.display.flip()
         
 
