6f3995639357c27f875ccd7676d5228e3ddefcdf
Nayeem Ahamed <nayeemar.ahamed@gmail.com>
2026-02-13 22:01:33 +0300

    Add asteroid spawning and player collision detection

--- main.py
+++ main.py
@@ -1,19 +1,28 @@
 import pygame
+import sys
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
-from logger import log_state
+from logger import log_state, log_event
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
-    Player.containers = (updatable, drawable)
-    dt = 0
-    player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
     asteroids = pygame.sprite.Group()
-    
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable,)
+
+    player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
+    asteroid_field = AsteroidField()
+    dt = 0
+
     while True:
         log_state()
         for event in pygame.event.get():
@@ -21,6 +30,12 @@ def main():
                 return
         dt = clock.tick(60)/1000
         updatable.update(dt)
+        for asteroid in asteroids:
+            if player.collides_with(asteroid):
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
+
         screen.fill("black")
         for dr in drawable:
             dr.draw(screen)
