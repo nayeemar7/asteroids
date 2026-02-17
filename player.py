9e479bc2e352cd5badc30438e6cbcb076c5ba298
Nayeem Ahamed <nayeemar.ahamed@gmail.com>
2026-02-17 20:59:06 +0300

    Add bullet shooting system

--- player.py
+++ player.py
@@ -1,6 +1,7 @@
 import pygame
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED, PLAYER_SPEED
+from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOOT_SPEED
+from shot import Shot
 
 class Player(CircleShape):
     def __init__(self,x,y):
@@ -32,6 +33,8 @@ class Player(CircleShape):
             self.move(dt)
         if keys[pygame.K_s]:
             self.move(-dt)
+        if keys[pygame.K_SPACE]:
+            self.shoot()
 
 
     def move(self,dt):
@@ -39,6 +42,12 @@ class Player(CircleShape):
         rotated_vector = unit_vector.rotate(self.rotation)
         rotated_with_speed_vector = rotated_vector * PLAYER_SPEED * dt
         self.position += rotated_with_speed_vector
+
+    def shoot(self):
+        shot =  Shot(self.position.x, self.position.y)
+        direction = pygame.Vector2(0,1)
+        direction = direction.rotate(self.rotation)
+        shot.velocity = direction*PLAYER_SHOOT_SPEED
 
 
 
